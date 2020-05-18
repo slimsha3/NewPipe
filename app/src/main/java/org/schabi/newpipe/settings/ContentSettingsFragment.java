702957541751bf6e50448714e119bd0ce27cca7e
polymorphicshade <polymorphicshade@gmail.com>
2020-05-18 14:42:25 -0600

    added ability to customize the base SponsorBlock API server url

--- app/src/main/java/org/schabi/newpipe/settings/ContentSettingsFragment.java
+++ app/src/main/java/org/schabi/newpipe/settings/ContentSettingsFragment.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
@@ -137,15 +136,6 @@ public class ContentSettingsFragment extends BasePreferenceFragment {
                     .putExtra(FilePickerActivityHelper.EXTRA_MODE,
                             FilePickerActivityHelper.MODE_DIR);
             startActivityForResult(i, REQUEST_EXPORT_PATH);
-            return true;
-        });
-
-        Preference sponsorblockStatusPreference =
-                findPreference(getString(R.string.sponsorblock_status));
-        sponsorblockStatusPreference.setOnPreferenceClickListener((Preference p) -> {
-            Intent i = new Intent(Intent.ACTION_VIEW,
-                    Uri.parse("https://status.sponsor.ajay.app/"));
-            startActivity(i);
             return true;
         });
     }
